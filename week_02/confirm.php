0fbbef749f9eb48e9dd30a6aa222e476b17c6f8c
mckennacs <48333899+mckennacs@users.noreply.github.com>
2023-02-06 17:43:00 -0700

    Module 3 Assignment Added New Line at end of HTML file and line separating PHP blocks

--- week_02/confirm.php
+++ week_02/confirm.php
@@ -6,11 +6,14 @@
   </head>
   <body>
     <?php
+
 //  Echo the form information from index.php based on their input name
     echo 'First name: ' . $_POST['firstname'].'<br />';
     echo 'Last name: ' . $_POST['lastname'].'<br />';
     echo 'Email: ' . $_POST['email'].'<br />';
     echo 'Comments: ' . $_POST['comment'].'<br />';
+
     ?>
   </body>
 </html>
+
