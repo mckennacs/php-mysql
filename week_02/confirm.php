b53780ca5ab609cf927247acf91762df45889f98
mckennacs <48333899+mckennacs@users.noreply.github.com>
2023-02-06 17:45:31 -0700

    Indented PHP lines

--- week_02/confirm.php
+++ week_02/confirm.php
@@ -8,10 +8,10 @@
     <?php
 
 //  Echo the form information from index.php based on their input name
-    echo 'First name: ' . $_POST['firstname'].'<br />';
-    echo 'Last name: ' . $_POST['lastname'].'<br />';
-    echo 'Email: ' . $_POST['email'].'<br />';
-    echo 'Comments: ' . $_POST['comment'].'<br />';
+      echo 'First name: ' . $_POST['firstname'].'<br />';
+      echo 'Last name: ' . $_POST['lastname'].'<br />';
+      echo 'Email: ' . $_POST['email'].'<br />';
+      echo 'Comments: ' . $_POST['comment'].'<br />';
 
     ?>
   </body>
