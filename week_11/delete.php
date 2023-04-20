62172f4bff3a84381a4f7e05e5bec1ddcc201c04
CIS166ae <122632455+CIS166ae@users.noreply.github.com>
2023-04-19 22:24:56 -0700

    Scott notes

--- week_11/delete.php
+++ week_11/delete.php
@@ -15,29 +15,35 @@ Module 12 Assignment
 <?php
 	// Includes MySQL database login info and LoginBox class
 	include_once 'includes/dbh.inc.php';
+	
+	 // DO NOT NEED AS YOUR NOT USING IT
 	include_once 'includes/LoginBox.php';
+	
 	// If connection not successful, outputs error message
-	if($conn->connect_error)
-	{
+	
+	// OPEN CURLY BRACKET ON SAME LINE
+	if($conn->connect_error) { 
 		die("Connection failed: " . $conn->connect_error);
 	}
 
 	// Checks to make sure $_GET['id'] is set
-  if(isset($_GET['id']))
-  {
+  if(isset($_GET['id'])) {
 		// Checks if id is an integer and a valid id number (greater than 0)
     $id = intval($_GET['id']);
-    if (is_int($id) && $id > 0){
+    
+		if (is_int($id) && $id > 0) {
 			// SQL query to delete user based on id
       $sql = "DELETE FROM `users` WHERE `id`=" . $id;
-      if (mysqli_query($conn, $sql)) {
+      
+			if (mysqli_query($conn, $sql)) {
 				// If delete is successful, redirects user to admin page after displaying success message
         header("Refresh:5, url=admin.php");
 				echo "<div class='success'>";
 				echo "<p>Record deleted.</p>";
 				echo "<p>You will be redirected to the admin page in 5 seconds, or click <a href='week_11/admin.php'>here</a></p>";
 				echo "</div>";
-      } else {
+      }
+			else {
 				// Prints error if $sql query doesn't work
         echo "ERROR: Could not execute $sql" . mysqli_error($conn);
       }
